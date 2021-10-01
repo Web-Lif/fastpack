be3b0a4f579a22eb922ec3020fc90b7a18b969a3
苯苯 <854363956@qq.com>
2021-10-02 05:12:43 +0800

    fix: 添加例子

--- src/preset/webpack.ts
+++ src/preset/webpack.ts
@@ -1,14 +1,18 @@
 import Config from 'webpack-chain'
 import CopyWebpackPlugin from 'copy-webpack-plugin'
 import HtmlWebpackPlugin from 'html-webpack-plugin'
-
+import { existsSync } from 'fs'
 import { DefinePlugin } from 'webpack'
 import { join } from 'path'
+
 import { FastPackConfig } from '../type'
 
 // eslint-disable-next-line import/prefer-default-export
 export function presetEntry (config: Config) {
-
+    const entry = join(process.cwd(), 'src', 'app.tsx');
+    config.entry('fastpack').add(entry).end();
+    config.output
+        .filename('[name].bundle.js');
     // see https://github.com/facebook/react/issues/2402
     // see https://github.com/facebook/react/issues/13991#issuecomment-435587809
     config.resolve.alias
@@ -24,9 +28,6 @@ export function presetLoader(config: Config) {
         .module
         .rule('fastpack/typescript')
         .test(/\.tsx?$/)
-        .include
-        .add('src')
-        .end()
         .use('fastpack/ts-loader')
         .loader('ts-loader')
         .end()
@@ -35,11 +36,30 @@ export function presetLoader(config: Config) {
 
 export function presetPlugins(config: Config,  {
     define = {},
-    copy = []
 }: FastPackConfig) {
     
     // see https://www.webpackjs.com/plugins/define-plugin/
     config.plugin('fastpack/DefinePlugin').use(DefinePlugin, [define])
+}
+
+export function presetDev(config: Config) {
+    config.mode('development');
+
+    const htmlWebpackPluginParam: HtmlWebpackPlugin.Options = {}
+
+    const document = join(process.cwd(), 'src', 'pages', 'document.ejs')
+    if (existsSync(document)) {
+        htmlWebpackPluginParam.template = document
+    }
+
+    // see https://www.webpackjs.com/plugins/html-webpack-plugin/
+    config.plugin('fastpack/HtmlWebpackPlugin').use(HtmlWebpackPlugin, [htmlWebpackPluginParam])
+}
+
+export function presetBuild(config: Config, {
+    copy = []
+}: FastPackConfig) {
+    config.mode('production')
 
     // see https://www.webpackjs.com/plugins/copy-webpack-plugin/
     config.plugin('fastpack/CopyWebpackPlugin').use(CopyWebpackPlugin, [{
@@ -48,18 +68,4 @@ export function presetPlugins(config: Config,  {
             ...copy
         ]
     }])
-
-    // see https://www.webpackjs.com/plugins/html-webpack-plugin/
-    config.plugin('fastpack/HtmlWebpackPlugin').use(HtmlWebpackPlugin, [{
-        template: join(process.cwd(), 'src', 'pages', 'document.ejs')
-    }])
-
-}
-
-export function presetDev(config: Config) {
-    config.mode('development');
-}
-
-export function presetBuild(config: Config) {
-    config.mode('production')
 }

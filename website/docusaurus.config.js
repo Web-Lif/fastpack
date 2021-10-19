b89527dc95142d5ad52489eb532849d60ab78cb8
苯苯 <854363956@qq.com>
2021-10-19 13:20:43 +0800

    fix: 修复文档编辑的错误

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -24,7 +24,7 @@ const config = {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
-          editUrl: 'https://github.com/Web-Lif/edit/canary/website/',
+          editUrl: 'https://github.com/Web-Lif/fastpack/edit/canary/website/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),

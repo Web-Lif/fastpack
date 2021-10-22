b93cd6408214bc6209e64dc85b60439b3255e347
苯苯 <854363956@qq.com>
2021-10-22 09:18:24 +0800

    fix: 修复和以前文件冲突的问题

--- packages/plugin-less-loader/src/index.ts
+++ packages/plugin-less-loader/src/index.ts
@@ -18,6 +18,20 @@ export default class FastpackPluginLessLoader {
             .end()
             .use('fastpack/css-loade')
             .loader('css-loader')
+            .end()
+            .use('fastpack/less-loader')
+            .loader('less-loader')
+            .options(this.options)
+            .end()
+        webpack
+            .module
+            .rule('fastpack/less')
+            .test(/module\.less$/)
+            .use('fastpack/style-loader')
+            .loader('style-loader')
+            .end()
+            .use('fastpack/css-loade')
+            .loader('css-loader')
             .options({
                 modules: true,
                 sourceMap: true,

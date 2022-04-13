20b55bd3b39a0d70db2b3007d2480c6708f13f15
苯苯 <91561865+robothot@users.noreply.github.com>
2022-04-13 13:43:41 +0800

    feat: 支持 React 18 (#90)

--- example/src/.fastpack/render.tsx
+++ example/src/.fastpack/render.tsx
@@ -1,24 +1,23 @@
 import React from "react";
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client';
+
 import Bootstrap, { routers, bootstrapRouters } from './frame'
 
 (window as any).g_routers = routers.concat(bootstrapRouters)
 
 if (document.querySelector('#root')) {
-    ReactDOM.render(
+    const root = createRoot(document.querySelector('#root'));
+    root.render(
         <Bootstrap routers={bootstrapRouters} />
-        ,
-        document.querySelector('#root')
     )
 } else {
-    const root = document.createElement("div")
-    root.setAttribute('id', 'root')
-    root.style.width = '100%'
-    root.style.height = '100%'
-    document.body.appendChild(root)
-    ReactDOM.render(
+    const rootDiv = document.createElement("div")
+    rootDiv.setAttribute('id', 'root')
+    rootDiv.style.width = '100%'
+    rootDiv.style.height = '100%'
+    document.body.appendChild(rootDiv)
+    const root = createRoot(rootDiv)
+    root.render(
         <Bootstrap routers={bootstrapRouters} />
-        ,
-        root
     )
 }

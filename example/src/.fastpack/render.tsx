591f14428a085cde9e8313fe403abd19f523e2a9
苯苯 <91561865+robothot@users.noreply.github.com>
2022-09-10 19:59:46 +0800

    feat: 添加 startup 文件用来控制启动项的信息 (#167)

--- example/src/.fastpack/render.tsx
+++ example/src/.fastpack/render.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { createRoot } from 'react-dom/client';
 
+import '@/startup'
+
 import Bootstrap, { routers, bootstrapRouters } from './frame'
 
 (window as any).g_routers = routers.concat(bootstrapRouters)
